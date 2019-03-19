2e72422179d82cf66a6bdef355d9e4b0820a7d7c
Abhishek Deshkar <deshkarabhishek@gmail.com>
2019-03-19 11:41:33 +0530

    Second Commit

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-data = pd.read_csv("data.csv")
+#Identifying NULL values.
+missing_values = ["Na","==","nA","NA"]
+
+data = pd.read_csv("data.csv",na_values=missing_values)
+
+#print(data)
 
 #Counts total null values.
 x1 = data['x1'].isnull().sum()
@@ -9,4 +14,8 @@ x3 = data['x3'].isnull().sum()
 x4 = data['x4'].isnull().sum()
 x5 = data['x5'].isnull().sum()
 
-print(x5)
+#Type of x3 column. int64
+print(data['x3'].dtype)
+
+#If there are any null values in data frame.
+print(data.isnull().values.any())
